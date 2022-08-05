6c857fb11c441306e88bea48574de6350f53aed8
Michael McDuffee <michael@whylabs.ai>
2022-08-05 09:41:12 -0400

    adding in the ability to armor block

--- src/engine/generateAttackResults.ts
+++ src/engine/generateAttackResults.ts
@@ -10,6 +10,7 @@ function generateEmptyAttackResult(resultClass: AttackResultClass) {
     weaponDamage: 0,
     armorDamage: 0,
     shieldDamage: 0,
+    fatigue: 0,
     resultClass,
   };
 }
@@ -31,6 +32,8 @@ export function generateAttackResults(
       atkResult.balanceLoss = 100 - attacker.getCorePhysical().balance; // Potentially also check for vulnerable status
       defResult.balanceLoss = 100 - defender.getCorePhysical().balance;
       defResult.delay = 100 - defender.getCorePhysical().quickness;
+      atkResult.fatigue = 100 - attacker.getCorePhysical().stamina;
+      defResult.fatigue = Math.max(100 - defender.getCorePhysical().balance - defender.getCorePhysical().stamina, 0);
       break;
     case 'Parry':
       atkResult.delay = 5;
@@ -42,14 +45,27 @@ export function generateAttackResults(
       defResult.balanceLoss = 5;
       defResult.delay = 2;
       break;
+    case 'Armor Block':
+      atkResult.delay = 5;
+      atkResult.balanceLoss = 2;
+      atkResult.weaponDamage = 2; // some function of the armor
+      atkResult.fatigue = 10;
+      defResult.armorDamage = 10; // some function of the armor
+      defResult.damage = 10; // vitality - armor rating
+      defResult.balanceLoss = 10;
+      defResult.delay = 10;
+      defResult.fatigue = 10;
+      break;
     case 'Clean Hit':
       atkResult.delay = 5;
       atkResult.balanceLoss = 2;
       atkResult.weaponDamage = 1;
+      atkResult.fatigue = 5;
       defResult.armorDamage = 10;
       defResult.damage = 20;
       defResult.balanceLoss = 10;
       defResult.delay = 10;
+      defResult.fatigue = 15;
       break;
     default:
       break;
